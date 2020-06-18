2675ca0ce2be872844c86d03d3837060b4caae7e
Bruno Parga <brunoparga@gmail.com>
2020-06-17 22:00:55 -0300

    Implement a basic Express server for the React app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const fetch = require('node-fetch')
+
+const app = express()
+app.use(cors({ origin: 'http://localhost:3002' }))
+
+app.get('/words', async (_, res) => {
+  const words = await fetch('http://localhost:3000/words').then((res) => res.json())
+  console.log(words)
+  res.json(words)
+})
+
+app.listen(3001)
