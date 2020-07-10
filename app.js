9b3f439a83b68e864f0d03389c7c3c3547eda90c
Bruno Parga <brunoparga@gmail.com>
2020-07-10 09:17:52 +0200

    Format the code more properly

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const fetch = require('node-fetch')
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
 
-const app = express()
-app.use(cors({ origin: 'http://localhost:3002' }))
+const app = express();
+app.use(cors({ origin: 'http://localhost:3002' }));
 
 app.get('/words', async (_, res) => {
   const words = await fetch('http://localhost:3000/words').then((res) => res.json())
   console.log(words)
   res.json(words)
-})
+});
 
-app.listen(3001)
+app.listen(3001);
