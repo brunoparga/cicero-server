efe814a3ee4a570a2c8d2340f3bcec15f57cf4b4
Bruno Parga <brunoparga@gmail.com>
2020-07-15 21:55:06 +0300

    Clean up the entire word list

--- utils/diederich.js
+++ utils/diederich.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
+// Include some words that the general rules wouldn't send to the right part of speech
+const {
+  adjectives,
+  advPreps,
+  conjunctions,
+  pronouns
+} = require('./manualList')
+
 // Ignore explanations and parts of speech in parentheses before the actual definition
 const treat = (string) => string.
   replace(/^(?:\(.*?\)[^\w(]*)+/, '')
@@ -7,22 +15,65 @@ const treat = (string) => string.
   // send the only one that's before it, 'tellus' (Earth) somewhere else.
   .replace(/^E/, 'e');
 
-// To sort by ascending alphabetical order of definition
+// Sort by ascending alphabetical order of definition
 const compare = ([_lemma1, definition1], [_lemma2, definition2]) => {
   const def1 = treat(definition1);
   const def2 = treat(definition2);
   return def1 === def2 ? 0 : def1 < def2 ? -1 : 1;
 }
 
-const classify = (lemma) => {
-  if (lemma.match(/(m|f|n)\.$/)) {
+// Fix the special words that the general rules get wrong
+const manualList = (lemma) => {
+  if (lemma === 'satis') {
+    return 'Adjective, Adverb';
+  } else if (["nec (neque)", "ubī (ubi)"].includes(lemma)) {
+    return 'Adverb, Conjunction'
+  } else if (lemma === "rēs pūblica, reī pūblicae, etc.") {
     return 'Noun';
-  } else if (lemma.split('-').length === 4) {
+  } else if (lemma === "mīlle (sg. indecl., pl. mīlia, -ium n.)") {
+    return 'Numeral';
+  } else if (lemma === "quatiō, -ere, --, quassum (perf. -cussī only in composita)") {
+    return 'Verb'
+  } else if (adjectives.includes(lemma)) {
+    return 'Adjective';
+  } else if (advPreps.includes(lemma)) {
+    return 'Adverb, Preposition';
+  } else if (conjunctions.includes(lemma)) {
+    return 'Conjunction';
+  } else if (pronouns.includes(lemma)) {
+    return 'Pronoun';
+  }
+};
+
+// Sort words out into their appropriate parts of speech
+const classify = (lemma, english) => {
+  const listed = manualList(lemma)
+  if (listed) {
+    return listed
+  } else if (lemma.match(/(m|f|n)\./)) {
+    return 'Noun';
+  } else if (lemma.split(',').length === 4 || treat(english).match(/^to /)) {
     return 'Verb';
   } else if (lemma.split(', -').length > 1) {
     return 'Adjective';
+  } else if (lemma.match(/(- |-$)/)) {
+    return 'Prefix';
+  } else if (lemma.match(/^-/)) {
+    return 'Particle'
+  } else if (english.includes('adv. and prep.')) {
+    return 'Adverb, Preposition'
+  } else if (english.includes('pron.')) {
+    return 'Pronoun'
+  } else if (english.includes('conj.')) {
+    return 'Conjunction'
+  } else if (english.toLowerCase().includes('prep.')) {
+    return 'Preposition'
+  } else if (english.includes('num.')) {
+    return 'Numeral'
+  } else if (english.includes('!')) {
+    return 'Interjection'
   }
-  return 'Etcetera';
+  return 'Adverb'
 };
 
 const words = fs
@@ -34,8 +85,15 @@ const words = fs
   .map((ary) => ary.split('\t').slice(4))
   // Alphabetize by cleaned-up definition
   .sort(compare)
+  // Remove duplicates
+  .filter(([lemma], index, array) =>
+    index === array.length - 1 ? true : lemma !== array[index + 1][0])
   // Turn into an object for JSONification
-  .map(([lemma, english]) => ({ partOfSpeech: classify(lemma), lemma, english, learned: false }));
+  .map(([lemma, english]) => ({
+    partOfSpeech: classify(lemma, english),
+    lemma,
+    english,
+    learned: false
+  }));
 
-// console.log(words2.map(({ lemma }) => lemma).sort((w1, w2) => w1 === w2 ? -1 : 1))
 fs.writeFileSync('./array.txt', JSON.stringify(words, null, 2))
---
+++ utils/manualList.js
@@ -0,0 +1,60 @@
+// Why each group of words is here
+
+exports.adjectives = [
+  // 'n.' classifies as noun
+  "alius, -a, -ud (gen. alterīus, dat. aliī/alterī)",
+  "alter, -era, -erum (gen. alterīus, dat. alterī)",
+  // definition beginning with 'to ' classifies as verb
+  "potior, -ius",
+  // 'num.' classifies as numeral
+  "quot (indecl.)",
+  // lack of hyphen in lemma classifies as adverb (default)
+  "ācer, ācris, ācre",
+  "āter, ātra, ātrum",
+  "dīs, dītis",
+  "māior, māius",
+  "minor, minus",
+  "pār, paris",
+  "plūs, plūris",
+  "prior, prius",
+]
+
+exports.advPreps = [
+  // The adverb and preposition meanings are listed separately
+  "ante",
+]
+
+exports.conjunctions = [
+  // Classified as adverbs
+  "igitur",
+  "nēve (neu)",
+  "quasi",
+  "sīve ... sīve",
+  "vel ... vel",
+]
+
+exports.pronouns = [
+  // Most of these classify as adverbs, a few as verbs because the gloss begins with 'to ' or
+  // due to commas splitting the lemma into four parts like verbs
+  "cui",
+  "egō (ego)",
+  "ille, illa, illud",
+  "ipse, ipsa, ipsum",
+  "mē",
+  "mihi",
+  "nēmō (nūllīus, nēminī, nēminem, nūllō)",
+  "nihil or nīl (indecl.)",
+  "nōbīs",
+  "nōs",
+  "quī, quis",
+  "quīlibet, quaelibet, quidlibet (subst., quodlibet, adj.)",
+  "quisnam, quidnam",
+  "quisquis, quidquid/quicquid (subst., quodquod, adj.)",
+  "sibi",
+  "tē",
+  "tibi",
+  "tū",
+  "uterque, utraque, utrumque",
+  "vōbīs",
+  "vōs",
+]
