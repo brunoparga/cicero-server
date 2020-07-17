eed8360666fbfe5bcf2c3bffff70dc502627b705
Bruno Parga <brunoparga@gmail.com>
2020-07-17 13:23:27 +0300

    Refactor Words class
    So far it only has one public method; that has been split into several
    private helper methods.

--- models/word.js
+++ models/word.js
@@ -4,28 +4,56 @@ const URI = 'postgres://bruno:Q8Pv8bX9xYZN2bx5SHLF8g3Bqcy@localhost:5432/cicero'
 const sequelize = new Sequelize(URI);
 
 class Word extends Model {
+  // Public interface
+  // Fetch words with their options and present them to controller
   static async fetch() {
-    const words = await this.findAll({
+    const words = await this.findAll(this.wordFindParams);
+    const wordsWithOptions = await Promise.all(words.map(this.addOptions.bind(this)));
+    return wordsWithOptions;
+  }
+
+  /*
+    Private methods
+  */
+
+  // SELECT only the relevant things from each drilled word to send to the front-end
+  static get wordFindParams() {
+    return {
+      // TODO: make this customizable
       limit: 10,
+      // TODO: remove this WHERE - this should find words of all parts of speech
+      // Requires populating the DB with the correct properties for every word
       where: { partOfSpeech: 'Adverb' },
       attributes: [['partOfSpeech', 'questionType'], 'lemma', 'english', 'learned', 'properties'],
-    });
-    const wordsWithOptions = await Promise.all(words.map(async (word) => {
-      const options = await this.findAll({
-        order: [Sequelize.literal('RANDOM()')],
-        limit: 3,
-        where: { partOfSpeech: word.dataValues.questionType },
-        attributes: ['lemma'],
-      });
-      return {
-        ...word.dataValues,
-        // Remove this to make teaching work
-        learned: true,
-        options: options.map((row) => row.dataValues.lemma),
-        properties: {},
-      };
-    }));
-    return wordsWithOptions;
+    };
+  }
+
+  // Find the options to add to each word
+  static async addOptions(word) {
+    const options = await this.findAll(this.optionsFindParams(word));
+    return this.buildWord(word, options);
+  }
+
+  // SELECT wrong options to accompany the correct word
+  // TODO: make this smarter than three random words from the same part of spech
+  static optionsFindParams(word) {
+    return {
+      order: [Sequelize.literal('RANDOM()')],
+      limit: 3,
+      where: { partOfSpeech: word.dataValues.questionType },
+      attributes: ['lemma'],
+    };
+  }
+
+  // Add the options to the word for dispatching to the front-end
+  static buildWord(word, options) {
+    return {
+      ...word.dataValues,
+      // Remove this to make teaching work
+      learned: true,
+      options: options.map((row) => row.dataValues.lemma),
+      properties: {},
+    };
   }
 }
 
