381938e1e43c8e22e56ede9fb213b5472cf1e629
Bruno Parga <brunoparga@gmail.com>
2020-07-18 14:21:22 +0300

    Fix most if not all issues with correct genitives

--- utils/readDiederich/setProperties.js
+++ utils/readDiederich/setProperties.js
@@ -1,12 +1,22 @@
 const setAdjectiveProperties = () => ({});
 
 const setDeclension = (lemma, number) => {
-  const singular = [/ae$/, /ī$/, /is$/, /ūs$/, /ēī$/];
+  const singular = [/ae$/, /ī$/, /is$/, /ūs$/, /(e|ē)ī$/];
   const plural = [/ārum$/, /ōrum$/, /[^(ā|ē|ō)r]um$/, /uum$/, /ērum$/];
-  const correctSuffixes = number === 'plural' ? plural : singular
+  const correctSuffixes = number === 'singular' ? singular : plural
   return correctSuffixes.reduce((declension, suffix, index) => {
     return lemma.split(', ')[1].match(suffix) ? index : declension
   }, 0)
+}
+
+const setCorrectGenitive = (lemma, number, declension) => {
+  const singular = ['-ae', '-ī', '-is', '-ūs', '-ēī'];
+  const plural = ['-ārum', '-ōrum', '-(i)um', '-uum', '-ērum'];
+  const regularGenitive = (number === 'singular' ? singular : plural)[declension]
+  if (lemma.split(', ')[1] !== regularGenitive) {
+    return lemma.split(', ')[1];
+  }
+  return undefined;
 }
 
 const setNounProperties = (lemma) => {
@@ -20,9 +30,7 @@ const setNounProperties = (lemma) => {
     gender = 'masculine';
   }
   const declension = setDeclension(lemma, number)
-  const singular = ['-ae', '-ī', '-is', '-ūs', '-ēī'];
-  const plural = ['-ārum', '-ōrum', '-(i)um', '-uum', '-ērum'];
-  const correctGenitive = (number === 'plural' ? plural : singular)[declension]
+  const correctGenitive = setCorrectGenitive(lemma, number, declension)
   return {
     number,
     declension,
