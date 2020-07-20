b75c8fff55e26ecdea6def8d6cc2cdb52b4f8c16
Bruno Parga <brunoparga@gmail.com>
2020-07-20 18:53:29 +0300

    Extract setNounProperties into its own file

--- utils/readDiederich/setProperties.js
+++ utils/readDiederich/setProperties.js
@@ -1,45 +1,6 @@
+const setNounProperties = require('./setNounProps');
+
 const setAdjectiveProperties = () => ({});
-
-const setDeclension = (lemma, number) => {
-  const singular = [/ae$/, /ī$/, /is$/, /ūs$/, /(e|ē)ī$/];
-  // Leaving the fourth and fifth declension endings here even though there are no plural
-  // words in those declensions in the Diederich list.
-  const plural = [/ārum$/, /ōrum$/, /[^(ā|ē|ō)r]um$/, /uum$/, /ērum$/];
-  const correctSuffixes = number === 'singular' ? singular : plural
-  return correctSuffixes.reduce((declension, suffix, index) => {
-    return lemma.split(', ')[1].match(suffix) ? index : declension
-  }, 0)
-}
-
-const setCorrectGenitive = (lemma, number, declension) => {
-  const singular = ['-ae', '-ī', '-is', '-ūs', '-ēī'];
-  const plural = ['-ārum', '-ōrum', '-(i)um', '-uum', '-ērum'];
-  const regularGenitive = (number === 'singular' ? singular : plural)[declension]
-  if (lemma.split(', ')[1] !== regularGenitive) {
-    return lemma.split(', ')[1];
-  }
-  return undefined;
-}
-
-const setNounProperties = (lemma) => {
-  const number = lemma.split(',')[1].match(/um$/) ? 'plural' : 'singular';
-  let gender = 'neuter';
-  if (lemma.includes('mf.')) {
-    gender = 'masculine/feminine';
-  } else if (lemma.includes('f.')) {
-    gender = 'feminine';
-  } else if (lemma.includes('m.')) {
-    gender = 'masculine';
-  }
-  const declension = setDeclension(lemma, number)
-  const correctGenitive = setCorrectGenitive(lemma, number, declension)
-  return {
-    number,
-    declension,
-    gender,
-    correctGenitive,
-  };
-};
 
 const setVerbProperties = () => ({});
 
