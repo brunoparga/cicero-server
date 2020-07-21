5385bab613682f22d6e5b7cbd7c30036f293261d
Bruno Parga <brunoparga@gmail.com>
2020-07-21 15:24:30 +0300

    Deal with six verbs with odd infinitives

--- utils/readDiederich/setVerbProps.js
+++ utils/readDiederich/setVerbProps.js
@@ -3,6 +3,11 @@ const setConjugation = (infinitive, deponent) => {
     const suffixes = [/ārī$/, /ērī$/, /([^((ā|ē|ī)r)]|mor)ī$/, /īrī$/];
     return suffixes.findIndex((suffix) => suffix.test(infinitive));
   }
+  // Take care of (circum)dō, coepī, meminī, edō, and fiō
+  if (infinitive.slice(-3) === 'are') { return 0; }
+  if (infinitive.slice(-4) === 'isse' || infinitive.includes(')')) { return 2; }
+  // TODO: what is left are sum, ferō, volō (weird irregular ones)
+  // also aio (3), inquam (irr)
   return ['āre', 'ēre', 'ere', 'īre'].indexOf(infinitive.slice(-3));
 };
 
@@ -10,6 +15,7 @@ const setCorrectInfinitive = (lemma, conjugation, deponent) => {
   // TODO: Verbs like ferō are not even getting here. I might have to actually tell the program
   // how a regular infinitive is formed, just so it can check these. What does get here are short
   // verbs whose infinitive is spelled out rather than use a hyphen.
+  // TODO: coepisse is listed but shouldn't, circumdare isn't but should
   if (!lemma.split(', ')[1].includes('-')) {
     return lemma.split(', ')[1];
   }
