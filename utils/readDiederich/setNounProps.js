88068f35288531ef4df0535f4cdefea1e5b6f321
Bruno Parga <brunoparga@gmail.com>
2020-08-04 20:24:05 +0300

    Refactor setNounProps

--- utils/readDiederich/setNounProps.js
+++ utils/readDiederich/setNounProps.js
@@ -1,3 +1,10 @@
+// The word 'mīlle', "1000", behaves weirdly enough to deserve its own props object defined
+const milleProps = {
+  number: 'plural',
+  declension: 2,
+  gender: 'neuter',
+};
+
 const setDeclension = (lemma, number) => {
   const singular = [/ae$/, /ī$/, /is$/, /ūs$/, /(e|ē)ī$/];
   // Leaving the fourth and fifth declension endings here even though there are no plural
@@ -9,8 +16,18 @@ const setDeclension = (lemma, number) => {
   );
 };
 
-// Some words, especially in the second and third declension, have slightly irregular
-// TODO: deal with words like culter (but not include puer in the same basket)
+const setGender = (lemma) => {
+  if (lemma.includes('mf.')) {
+    return 'masculine/feminine';
+  } if (lemma.includes('f.')) {
+    return 'feminine';
+  } if (lemma.includes('m.')) {
+    return 'masculine';
+  }
+  return 'neuter';
+};
+
+// Some words, especially in the second and third declension, have slightly irregular genitives
 const setCorrectGenitive = (lemma, number, declension) => {
   const singular = ['-ae', '-ī', '-is', '-ūs', '-ēī'];
   const plural = ['-ārum', '-ōrum', '-(i)um', '-uum', '-ērum'];
@@ -21,34 +38,17 @@ const setCorrectGenitive = (lemma, number, declension) => {
   return undefined;
 };
 
-const milleProps = {
-  number: 'plural',
-  declension: 2,
-  gender: 'neuter',
-};
-
 module.exports = (lemma) => {
-  if (lemma === 'mīlle (sg. indecl., pl. mīlia, -ium n.)') {
-    return milleProps;
-  }
+  if (lemma === 'mīlle (sg. indecl., pl. mīlia, -ium n.)') { return milleProps; }
+
   const number = /um$/.test(lemma.split(',')[1]) ? 'plural' : 'singular';
-
-  let gender = 'neuter';
-  if (lemma.includes('mf.')) {
-    gender = 'masculine/feminine';
-  } else if (lemma.includes('f.')) {
-    gender = 'feminine';
-  } else if (lemma.includes('m.')) {
-    gender = 'masculine';
-  }
-
   const declension = setDeclension(lemma, number);
   const correctGenitive = setCorrectGenitive(lemma, number, declension);
 
   return {
     number,
     declension,
-    gender,
+    gender: setGender(lemma),
     correctGenitive,
   };
 };
