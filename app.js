210fb11f8ad8c5526f65e1115eedd3e8a838bdbf
Bruno Parga <brunoparga@gmail.com>
2020-09-04 17:08:18 +0300

    Change front-end address to allow CORS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const compression = require('compression');
 const Word = require('./models/word');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://master.d2ylwpfde239og.amplifyapp.com/' }));
 app.use(helmet());
 app.use(compression());
 
