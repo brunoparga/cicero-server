b7b9d2e769bce55ed18c1cfc501e164d071969c7
Bruno Parga <brunoparga@gmail.com>
2020-09-14 19:33:21 +0300

    Reorganize the DB interface

--- models/word.js
+++ models/word.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
 const {
   Sequelize, Model, DataTypes, Op,
 } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 class Word extends Model {
   // Public interface
