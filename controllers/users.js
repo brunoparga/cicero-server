7eae9f6a0a5970b406e3e62f090fa827a449bad0
Bruno Parga <brunoparga@gmail.com>
2020-09-20 13:47:50 +0000

    Improve signup handling

--- controllers/users.js
+++ controllers/users.js
@@ -5,14 +5,22 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.signup = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    return res.status(403).json({ message: "Passwords don't match." });
+  }
   const user = await User.findOne({ where: { email } });
   if (user) {
-    return res.status(403).json({ message: 'Account exists. Please login.' });
+    return res.status(403).json({ message: 'Account already exists. Please login.' });
   }
   const hashedPw = await bcrypt.hash(password, 12);
   const newUser = await User.create({ email, password: hashedPw });
-  return res.status(201).json(newUser);
+  const token = jwt.sign(
+    { id: newUser.id },
+    process.env.TOKEN_SIGNING_SECRET,
+    { expiresIn: '1h' },
+  );
+  return res.status(201).json({ email, token });
 };
 
 exports.signin = async (req, res) => {
