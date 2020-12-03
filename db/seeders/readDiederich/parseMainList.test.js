f5aa18304cae019465bc9fa16a07d588d72bf022
Bruno Parga <brunoparga@gmail.com>
2020-12-03 20:42:00 +0300

    Test the main seeder function

---
+++ db/seeders/readDiederich/parseMainList.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable node/no-sync */
+"use strict";
+
+/**
+ * Unit tests for the function that builds the JSON blueprint for the DB seed
+ *
+ * @group unit
+ */
+
+const fs = require("fs");
+const path = require("path");
+
+const parseMainList = require("./parseMainList");
+
+describe("parseMainList", () => {
+  it("creates a file named ../db.json", () => {
+    const filename = path.join(__dirname, "../db.json");
+
+    fs.rmSync(filename);
+    expect(fs.existsSync(filename)).toBeFalsy();
+    parseMainList();
+    expect(fs.existsSync(filename)).toBeTruthy();
+  });
+});
