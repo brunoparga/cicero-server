379a01c46060827989018fd6be4361a15ea5b7ed
Bruno Parga <brunoparga@gmail.com>
2020-12-04 12:09:05 +0300

    Improve test of seed parsing function

--- db/seeders/readDiederich/parseMainList.test.js
+++ db/seeders/readDiederich/parseMainList.test.js
@@ -13,11 +13,15 @@ const path = require("path");
 const parseMainList = require("./parseMainList");
 
 describe("parseMainList", () => {
-  it("creates a file named ../db.json", () => {
+  it("idempotently creates a file named ../db.json", () => {
     const filename = path.join(__dirname, "../db.json");
 
     fs.rmSync(filename);
     expect(fs.existsSync(filename)).toBeFalsy();
+    parseMainList();
+    expect(fs.existsSync(filename)).toBeTruthy();
+
+    // Repeating the assertion tests idempotence
     parseMainList();
     expect(fs.existsSync(filename)).toBeTruthy();
   });
